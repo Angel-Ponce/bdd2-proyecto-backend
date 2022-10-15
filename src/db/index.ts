5e804f158aef8864190d45415e04dbf7c70e44e4
Angel Ponce <acdpf000@gmail.com>
2022-10-15 10:56:13 -0600

    configure database conection

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,1 +1,12 @@
-export {};
+import { Sequelize } from "sequelize-typescript";
+
+const db = new Sequelize({
+  database: process.env.DB_DATABASE,
+  dialect: "mssql",
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT || 1433),
+});
+
+export { db };
