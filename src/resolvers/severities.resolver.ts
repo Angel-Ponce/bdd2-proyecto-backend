c40eacb55227a0f10aee353ae89ff837c093a50a
Angel Ponce <acdpf000@gmail.com>
2022-10-20 17:26:49 -0600

    crud for severities

--- src/resolvers/severities.resolver.ts
+++ src/resolvers/severities.resolver.ts
@@ -1,6 +1,95 @@
+import { GraphQLError } from "graphql";
+import { Args, Context } from "@types";
+import { exec } from "@helpers";
+
 const severityResolver = {
-  Query: {},
-  Mutation: {},
+  Query: {
+    severities: async (_o: any, _args: Args, context: Context) => {
+      if (!context.user) throw new GraphQLError("Sin autenticación.");
+
+      const [data, error] = await exec("allSeverities");
+
+      if (error) throw error;
+
+      return data;
+    },
+
+    severity: async (_o: any, args: Args, context: Context) => {
+      if (!context.user) throw new GraphQLError("Sin autenticación");
+
+      const [data, error] = await exec(
+        "getSeverityById ?",
+        [args.input.id || 0],
+        false
+      );
+
+      if (error) throw error;
+
+      return data;
+    },
+  },
+
+  Mutation: {
+    createSeverity: async (_o: any, args: Args, context: Context) => {
+      if (!context.user) throw new GraphQLError("Sin autenticación");
+
+      const [data, error] = await exec(
+        "createSeverity ?",
+        [args.input.name || ""],
+        false
+      );
+
+      if (error) throw error;
+
+      const [severity, error2] = await exec(
+        "getSeverityById ?",
+        [data.id || 0],
+        false
+      );
+
+      if (error2) throw error2;
+
+      return severity;
+    },
+
+    updateSeverity: async (_o: any, args: Args, context: Context) => {
+      if (!context.user) throw new GraphQLError("Sin autenticación");
+
+      const [data, error] = await exec(
+        "updateSeverity ?, ?",
+        [args.input.id || 0, args.input.name || ""],
+        false
+      );
+
+      if (error) throw error;
+
+      const [severity, error2] = await exec(
+        "getSeverityById ?",
+        [data.id || 0],
+        false
+      );
+
+      if (error2) throw error2;
+
+      return severity;
+    },
+
+    deleteSeverity: async (_o: any, args: Args, context: Context) => {
+      if (!context.user) throw new GraphQLError("Sin autenticación");
+
+      const [data, error] = await exec(
+        "deleteSeverity ?",
+        [args.input.id || 0],
+        false
+      );
+
+      if (error) throw error;
+
+      if (data.result) return { message: "Severidad eliminada correctamente." };
+
+      return { message: "Severidad no eliminada" };
+    },
+  },
 };
 
 export { severityResolver };
--- src/schemas/severities.schema.ts
+++ src/schemas/severities.schema.ts
@@ -1,3 +1,28 @@
-const severitySchema = /* GraphQL */ ``;
+const severitySchema = /* GraphQL */ `
+  type Severity {
+    id: Int!
+    name: String!
+  }
+
+  input CreateSeverityInput {
+    name: String!
+  }
+
+  input UpdateSeverityInput {
+    id: Int!
+    name: String!
+  }
+
+  extend type Query {
+    severities: [Severity!]!
+    severity(input: IdInput!): Severity!
+  }
+
+  extend type Mutation {
+    createSeverity(input: CreateSeverityInput!): Severity!
+    updateSeverity(input: UpdateSeverityInput!): Severity!
+    deleteSeverity(input: IdInput!): Message!
+  }
+`;
 
 export { severitySchema };
