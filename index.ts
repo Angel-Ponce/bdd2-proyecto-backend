30c368fc94ac23a589e74ddc1e76201dfb3de919
Angel Ponce <acdpf000@gmail.com>
2022-10-21 14:47:29 -0600

    using express

--- index.ts
+++ index.ts
@@ -7,34 +7,48 @@ import { resolvers } from "@resolvers/index";
 import { makeExecutableSchema } from "graphql-tools";
 import { exec } from "@helpers";
 import { User } from "@types";
+import cors from "cors";
+import express from "express";
+import http from "http";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import bodyParser from "body-parser";
+import { expressMiddleware } from "@apollo/server/express4";
+
+const app = express();
+const httpServer = http.createServer(app);
 
 const server = new ApolloServer({
-  schema: makeExecutableSchema({
-    typeDefs,
-    resolvers,
-  }),
+  schema: makeExecutableSchema({ typeDefs, resolvers }),
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-startStandaloneServer(server, {
-  listen: {
-    port: Number(process.env.PORT || 4010),
-    // host: process.env.APP_HOST || "localhost",
-  },
-  context: async ({ req }) => {
-    const token = req.headers.authorization || "";
-
-    const [data, error] = await exec("getUserByToken ?", [token], false);
-
-    let user: User | null = null;
-
-    if (error) {
-      return { user: null };
-    }
-
-    user = data;
-
-    return { user };
-  },
-}).then(({ url }) => {
-  console.log(`🚀 Server ready at: ${url}`);
+server.start().then(async () => {
+  app.use(cors(), bodyParser.json(), expressMiddleware(server));
+  await new Promise((resolve: any) =>
+    httpServer.listen({ port: process.env.PORT || 4010 }, resolve)
+  );
 });
+
+// startStandaloneServer(server, {
+//   listen: {
+//     port: Number(process.env.PORT || 4010),
+//     // host: process.env.APP_HOST || "localhost",
+//   },
+//   context: async ({ req }) => {
+//     const token = req.headers.authorization || "";
+
+//     const [data, error] = await exec("getUserByToken ?", [token], false);
+
+//     let user: User | null = null;
+
+//     if (error) {
+//       return { user: null };
+//     }
+
+//     user = data;
+
+//     return { user };
+//   },
+// }).then(({ url }) => {
+//   console.log(`🚀 Server ready at: ${url}`);
+// });
