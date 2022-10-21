9cbd63ca4f3137fc4fa2e455ab2f64aee3803719
Angel Ponce <acdpf000@gmail.com>
2022-10-21 15:43:55 -0600

    ...

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,11 +4,11 @@ import * as tedious from "tedious";
 const db = new Sequelize({
   database: process.env.DB_DATABASE,
   dialect: "mssql",
+  dialectModule: tedious,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT || 1433),
-  dialectModule: tedious,
 });
 
 export { db };
