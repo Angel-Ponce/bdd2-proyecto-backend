42747034c0bb1a278ab0dd1db607a44a07be6794
Angel Ponce <acdpf000@gmail.com>
2022-10-21 23:41:04 -0600

    building

--- dist/index.js
+++ dist/index.js
@@ -29,12 +29,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv = __importStar(require("dotenv"));
 dotenv.config();
 require("reflect-metadata");
-const server_1 = require("@apollo/server");
+const apollo_server_express_1 = require("apollo-server-express");
 const index_1 = require("./src/schemas/index");
 const index_2 = require("./src/resolvers/index");
 const graphql_tools_1 = require("graphql-tools");
-const drainHttpServer_1 = require("@apollo/server/plugin/drainHttpServer");
-const express4_1 = require("@apollo/server/express4");
+const _helpers_1 = require("./src/helpers");
+const apollo_server_core_1 = require("apollo-server-core");
 const express_1 = __importDefault(require("express"));
 const http_1 = __importDefault(require("http"));
 const cors_1 = __importDefault(require("cors"));
@@ -46,19 +46,26 @@ const initServer = async () => {
     app.use((0, cors_1.default)());
     app.use(body_parser_1.default.json());
     const httpServer = http_1.default.createServer(app);
-    const server = new server_1.ApolloServer({
+    const server = new apollo_server_express_1.ApolloServer({
         schema: (0, graphql_tools_1.makeExecutableSchema)({
             typeDefs: index_1.typeDefs,
             resolvers: index_2.resolvers,
         }),
-        plugins: [
-            // ApolloServerPluginLandingPageProductionDefault(),
-            (0, drainHttpServer_1.ApolloServerPluginDrainHttpServer)({ httpServer }),
-        ],
+        plugins: [(0, apollo_server_core_1.ApolloServerPluginDrainHttpServer)({ httpServer })],
         introspection: true,
+        context: async ({ req }) => {
+            const token = req.headers.authorization || "";
+            const [data, error] = await (0, _helpers_1.exec)("getUserByToken @0", [token], false);
+            let user = null;
+            if (error) {
+                return { user: null };
+            }
+            user = data;
+            return { user };
+        },
     });
     await server.start();
-    app.use("/graphql", (0, express4_1.expressMiddleware)(server));
+    server.applyMiddleware({ app });
     await new Promise((resolve) => httpServer.listen({ port: process.env.PORT || 4010 }, resolve));
 };
 initServer();
