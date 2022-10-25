d1fe4c7017ad7c9c9753ccf2b6bf4d6b37b1d898
Angel Ponce <acdpf000@gmail.com>
2022-10-25 08:54:42 -0600

    messages query

--- src/resolvers/messages.resolver.ts
+++ src/resolvers/messages.resolver.ts
@@ -1,5 +1,47 @@
+import { GraphQLError } from "graphql";
+import { Args, Context, Parent } from "@types";
+import { exec } from "@helpers";
+
 const messageResolver = {
-  Query: {},
+  Query: {
+    messages: async (_o: any, args: Args, context: Context) => {
+      if (!context.user) throw new GraphQLError("Sin autenticación");
+
+      const [data, error] = await exec("allTicketMessages @0", [
+        args.input.id || 0,
+      ]);
+
+      if (error) throw error;
+
+      return data;
+    },
+  },
+
+  ChatMessage: {
+    user: async (parent: Parent) => {
+      const [user, error] = await exec(
+        "getUserById @0",
+        [parent.userId || 0],
+        false
+      );
+
+      if (error) throw error;
+
+      return user;
+    },
+
+    ticket: async (parent: Parent) => {
+      const [ticket, error] = await exec(
+        "getTicketById @0",
+        [parent.ticketId || 0],
+        false
+      );
+
+      if (error) throw error;
+
+      return ticket;
+    },
+  },
   Mutation: {},
 };
 
--- src/schemas/messages.schema.ts
+++ src/schemas/messages.schema.ts
@@ -1,3 +1,17 @@
-const messageSchema = /* GraphQL */ ``;
+const messageSchema = /* GraphQL */ `
+  scalar DateTime
+
+  type ChatMessage {
+    id: Int!
+    message: String!
+    user: User!
+    ticket: Ticket!
+    createdAt: DateTime
+  }
+
+  extend type Query {
+    messages(input: IdInput!): [ChatMessage!]!
+  }
+`;
 
 export { messageSchema };
