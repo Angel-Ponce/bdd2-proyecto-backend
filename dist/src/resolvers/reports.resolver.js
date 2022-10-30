6de059bc43ed1315d5f75b7059641567ae098a06
Angel Ponce <acdpf000@gmail.com>
2022-10-30 11:09:43 -0600

    building

--- dist/src/resolvers/reports.resolver.js
+++ dist/src/resolvers/reports.resolver.js
@@ -1,9 +1,51 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.reportResolver = void 0;
+const graphql_1 = require("graphql");
+const _helpers_1 = require("../helpers");
 const reportResolver = {
-    Query: {},
-    Mutation: {},
+    Query: {
+        reports: async (_o, _args, context) => {
+            if (!context.user)
+                throw new graphql_1.GraphQLError("Sin autenticación.");
+            const [ticketsByCategory, e1] = await (0, _helpers_1.exec)("ticketsByCategory", []);
+            if (e1)
+                throw e1;
+            const [ticketsBySeverity, e2] = await (0, _helpers_1.exec)("ticketsBySeverity", []);
+            if (e2)
+                throw e2;
+            const [ticketsByStatus, e3] = await (0, _helpers_1.exec)("ticketsByStatus", []);
+            if (e3)
+                throw e3;
+            const [usersHistory, e4] = await (0, _helpers_1.exec)("usersHistory", []);
+            if (e4)
+                throw e4;
+            const [userWithMoreTicketsResolved, e5] = await (0, _helpers_1.exec)("userWithMoreTicketsResolved", [], false);
+            if (e5)
+                throw e5;
+            const [userWithMoreTicketsReported, e6] = await (0, _helpers_1.exec)("userWithMoreTicketsReported", [], false);
+            if (e6)
+                throw e6;
+            const [userWithLessTicketsResolved, e7] = await (0, _helpers_1.exec)("userWithLessTicketsResolved", [], false);
+            if (e7)
+                throw e7;
+            const [userWithLessTicketsReported, e8] = await (0, _helpers_1.exec)("userWithLessTicketsReported", [], false);
+            if (e8)
+                throw e8;
+            return {
+                ticketsByCategory,
+                ticketsBySeverity,
+                ticketsByStatus,
+                usersHistory,
+                kpis: {
+                    userWithMoreTicketsReported,
+                    userWithMoreTicketsResolved,
+                    userWithLessTicketsReported,
+                    userWithLessTicketsResolved,
+                },
+            };
+        },
+    },
 };
 exports.reportResolver = reportResolver;
 //# sourceMappingURL=reports.resolver.js.map
--- dist/src/schemas/reports.schema.js
+++ dist/src/schemas/reports.schema.js
@@ -1,6 +1,66 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.reportSchema = void 0;
-const reportSchema = /* GraphQL */ ``;
+const reportSchema = /* GraphQL */ `
+  type TicketsByCategoryReport {
+    category: String!
+    ticketsCount: Int!
+  }
+
+  type TicketsBySeverityReport {
+    severity: String!
+    ticketsCount: Int!
+  }
+
+  type TicketsByStatusReport {
+    status: String!
+    ticketsCount: Int!
+  }
+
+  type UsersHistoryReport {
+    user: String!
+    ticketsResolved: Int!
+    ticketsReported: Int!
+  }
+
+  type UserWithMoreTicketsResolved {
+    user: String!
+    ticketsResolved: Int!
+  }
+
+  type UserWithMoreTicketsReported {
+    user: String!
+    ticketsReported: Int!
+  }
+
+  type UserWithLessTicketsResolved {
+    user: String!
+    ticketsResolved: Int!
+  }
+
+  type UserWithLessTicketsReported {
+    user: String!
+    ticketsReported: Int!
+  }
+
+  type KPIS {
+    userWithMoreTicketsReported: UserWithMoreTicketsReported
+    userWithMoreTicketsResolved: UserWithMoreTicketsResolved
+    userWithLessTicketsReported: UserWithLessTicketsReported
+    userWithLessTicketsResolved: UserWithLessTicketsResolved
+  }
+
+  type Reports {
+    ticketsByCategory: [TicketsByCategoryReport!]!
+    ticketsBySeverity: [TicketsBySeverityReport!]!
+    ticketsByStatus: [TicketsByStatusReport!]!
+    usersHistory: [UsersHistoryReport!]!
+    kpis: KPIS!
+  }
+
+  extend type Query {
+    reports: Reports!
+  }
+`;
 exports.reportSchema = reportSchema;
 //# sourceMappingURL=reports.schema.js.map
