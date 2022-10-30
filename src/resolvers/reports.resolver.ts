7d2d1cf9a33eadcb58dab52d80896955838541d3
Angel Ponce <acdpf000@gmail.com>
2022-10-30 11:00:49 -0600

    reports

--- src/resolvers/reports.resolver.ts
+++ src/resolvers/reports.resolver.ts
@@ -1,6 +1,36 @@
+import { GraphQLError } from "graphql";
+import { Args, Context } from "@types";
+import { exec } from "@helpers";
+
 const reportResolver = {
-  Query: {},
-  Mutation: {},
+  Query: {
+    reports: async (_o: any, _args: Args, context: Context) => {
+      if (!context.user) throw new GraphQLError("Sin autenticación.");
+
+      const [ticketsByCategory, e1] = await exec("ticketsByCategory", []);
+
+      if (e1) throw e1;
+
+      const [ticketsBySeverity, e2] = await exec("ticketsBySeverity", []);
+
+      if (e2) throw e2;
+
+      const [ticketsByStatus, e3] = await exec("ticketsByStatus", []);
+
+      if (e3) throw e3;
+
+      const [usersHistory, e4] = await exec("usersHistory", []);
+
+      if (e4) throw e4;
+
+      return {
+        ticketsByCategory,
+        ticketsBySeverity,
+        ticketsByStatus,
+        usersHistory,
+      };
+    },
+  },
 };
 
 export { reportResolver };
--- src/schemas/reports.schema.ts
+++ src/schemas/reports.schema.ts
@@ -1,3 +1,35 @@
-const reportSchema = /* GraphQL */ ``;
+const reportSchema = /* GraphQL */ `
+  type TicketsByCategoryReport {
+    category: String!
+    ticketsCount: Int!
+  }
+
+  type TicketsBySeverityReport {
+    severity: String!
+    ticketsCount: Int!
+  }
+
+  type TicketsByStatusReport {
+    status: String!
+    ticketsCount: Int!
+  }
+
+  type UsersHistoryReport {
+    user: String!
+    ticketsResolved: Int!
+    ticketsReported: Int!
+  }
+
+  type Reports {
+    ticketsByCategory: [TicketsByCategoryReport!]!
+    ticketsBySeverity: [TicketsBySeverityReport!]!
+    ticketsByStatus: [TicketsByStatusReport!]!
+    usersHistory: [UsersHistoryReport!]!
+  }
+
+  extend type Query {
+    reports: Reports!
+  }
+`;
 
 export { reportSchema };
